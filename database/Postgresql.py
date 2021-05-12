6f92e88cc15ec3710c2c5a96319aa57ee6065823
newmarwegner <newmarwegner@gmail.com>
2021-05-12 10:18:00 -0300

    get dictionary

--- database/Postgresql.py
+++ database/Postgresql.py
@@ -10,14 +10,27 @@ class Postgres:
         self.table = 'logged_actions'
     
     def conn_postgres(self):
+        """
+        Function to connect with postgresql database considering url inside of .env file
+        :return: return a connection with postgresql
+        """
         self.conn = create_engine(self.db_connection_url)
         return self.conn
     
     def schemaname(self, schema):
+        """
+        :param schema: schema with audit logs
+        :return: schema_name to be use on processes
+        """
         self.schema_name = schema
     
     def select_postgres(self, days=0, columns='*'):
-        
+        """
+        Function to return all rows of audit_logs postgres considering inserts, updates and deletes
+        :param days: Last days to filter. The 0 parameter return all rows
+        :param columns: Can be used to another selection in postgres, to audit_logs need to be *
+        :return: Return all rows selected
+        """
         if columns != '*':
             columns = ','.join(columns)
         
@@ -36,11 +49,46 @@ class Postgres:
         :return: List filtered with param choice given
         """
         return [t for t in self.select_postgres(days) if t[5] == choice]
+    
+    def get_alters(self, days):
+        """
+        Function to get a dictionary of table names and primary key of inserts, deletes and updates
+        :param days: Days to be used as filter on last days
+        :return: Dictionary with schemaname, table, operation and primary keys
+        """
+        update = self.auditoperation_postgres('U', days)
+        delete = self.auditoperation_postgres('D', days)
+        inserts = self.auditoperation_postgres('I', days)
+        result = update + delete + inserts
 
+        lista = []
+        for i in result:
+            table_name = '.'.join(i[1:3])
+            if not i[5] == 'D':
+                values = i[7][1:-2].split(',')
+                lista.append([table_name, values[0], i[5]])
+            else:
+                values = i[6][1:-2].split(',')
+                lista.append([table_name, values[0], i[5]])
+
+        dicionario = {}
+        for i in lista:
+            key = i[0]
+            id_pk = i[1]
+            op = i[2]
+            if key not in dicionario:
+                dicionario[key] = {
+                    op: [str(id_pk)]
+                }
+            elif op not in dicionario[key]:
+                dicionario[key][op] = [str(id_pk)]
+            else:
+                dicionario[key][op].append(str(id_pk))
+
+        return dicionario
 
 
 if __name__ == '__main__':
     pg = Postgres()
-    for i in pg.auditoperation_postgres('I', 7):
-        print(i)
-    #TODO: Create function to join inserts and updates
+    tables = pg.get_alters(1)
+    print(tables)
