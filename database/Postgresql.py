fe56144f2eb8b7acf0aa196119e681c7ded4ecec
newmarwegner <newmarwegner@gmail.com>
2021-05-18 14:01:24 -0300

    final

--- database/Postgresql.py
+++ database/Postgresql.py
@@ -8,7 +8,7 @@ class Postgres:
         self.conn = None
         self.schema_name = 'audit'
         self.table = 'logged_actions'
-    
+
     def conn_postgres(self):
         """
         Function to connect with postgresql database considering url inside of .env file
@@ -16,14 +16,14 @@ class Postgres:
         """
         self.conn = create_engine(self.db_connection_url)
         return self.conn
-    
+
     def schemaname(self, schema):
         """
         :param schema: schema with audit logs
         :return: schema_name to be use on processes
         """
         self.schema_name = schema
-    
+
     def select_postgres(self, days=0, columns='*'):
         """
         Function to return all rows of audit_logs postgres considering inserts, updates and deletes
@@ -33,15 +33,15 @@ class Postgres:
         """
         if columns != '*':
             columns = ','.join(columns)
-        
+
         select = f"select {columns} from {self.schema_name}.{self.table}"
-        
+
         if days != 0:
             select = select + f" where action_tstamp > current_date - interval '{days} days'"
         self.conn_postgres()
-        
+
         return self.conn.execute(select).fetchall()
-    
+
     def auditoperation_postgres(self, choice, days=0):
         """
         :param days: Amount of days to filter
@@ -49,30 +49,47 @@ class Postgres:
         :return: List filtered with param choice given
         """
         return [t for t in self.select_postgres(days) if t[5] == choice]
-    
-    def get_alters(self, days):
+
+    def concat_alters(self, days):
         """
-        Function to get a dictionary of table names and primary key of inserts, deletes and updates
+        Function to concat in list all updates, deletes, and inserts find in audit_logs
         :param days: Days to be used as filter on last days
-        :return: Dictionary with schemaname, table, operation and primary keys
+        :return: List of concat operation in a specific filter dates
         """
         update = self.auditoperation_postgres('U', days)
         delete = self.auditoperation_postgres('D', days)
         inserts = self.auditoperation_postgres('I', days)
-        result = update + delete + inserts
 
-        lista = []
-        for i in result:
+        return update + delete + inserts
+
+    def list_to_dictionary(self, list_alters):
+        """
+        Function to get a list to be convert as dictionary final result
+        :param list_alters: param from function concat_alters
+        :return: a list with table names, primary keys and operations
+        """
+        list_to_dictionary = []
+        for i in list_alters:
             table_name = '.'.join(i[1:3])
             if not i[5] == 'D':
                 values = i[7][1:-2].split(',')
-                lista.append([table_name, values[0], i[5]])
+                list_to_dictionary.append([table_name, values[0], i[5]])
             else:
                 values = i[6][1:-2].split(',')
-                lista.append([table_name, values[0], i[5]])
+                list_to_dictionary.append([table_name, values[0], i[5]])
 
+        return list_to_dictionary
+
+
+    def get_alters(self, list_alters):
+        """
+        Function to get a dictionary of table names and primary key of inserts, deletes and updates
+        :param list_alters: List of all inserts, deletes and updates concat
+        :return: Dictionary with schemaname, table, operation and primary keys
+        """
+        list_to_dictionary = self.list_to_dictionary(list_alters)
         dicionario = {}
-        for i in lista:
+        for i in list_to_dictionary:
             key = i[0]
             id_pk = i[1]
             op = i[2]
@@ -90,5 +107,7 @@ class Postgres:
 
 if __name__ == '__main__':
     pg = Postgres()
-    tables = pg.get_alters(1)
-    print(tables)
+    list_alters = pg.concat_alters(20)
+    tables = pg.get_alters(list_alters)
+    for item in tables.items():
+        print(item)
