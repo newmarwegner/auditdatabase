94f294f6a759ec2d94831681169bbfdafd0eb0cf
newmarwegner <newmarwegner@gmail.com>
2021-05-19 16:16:21 -0300

    insert database

--- database/Postgresql.py
+++ database/Postgresql.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, Boolean, Date
 from decouple import config
+from sqlalchemy.dialects import postgresql
+from datetime import date
 
 
 class Postgres:
@@ -8,7 +10,8 @@ class Postgres:
         self.conn = None
         self.schema_name = 'audit'
         self.table = 'logged_actions'
-
+        self.sync_table = None
+    
     def conn_postgres(self):
         """
         Function to connect with postgresql database considering url inside of .env file
@@ -16,14 +19,14 @@ class Postgres:
         """
         self.conn = create_engine(self.db_connection_url)
         return self.conn
-
+    
     def schemaname(self, schema):
         """
         :param schema: schema with audit logs
         :return: schema_name to be use on processes
         """
         self.schema_name = schema
-
+    
     def select_postgres(self, days=0, columns='*'):
         """
         Function to return all rows of audit_logs postgres considering inserts, updates and deletes
@@ -33,15 +36,15 @@ class Postgres:
         """
         if columns != '*':
             columns = ','.join(columns)
-
+        
         select = f"select {columns} from {self.schema_name}.{self.table}"
-
+        
         if days != 0:
             select = select + f" where action_tstamp > current_date - interval '{days} days'"
         self.conn_postgres()
-
+        
         return self.conn.execute(select).fetchall()
-
+    
     def auditoperation_postgres(self, choice, days=0):
         """
         :param days: Amount of days to filter
@@ -49,7 +52,7 @@ class Postgres:
         :return: List filtered with param choice given
         """
         return [t for t in self.select_postgres(days) if t[5] == choice]
-
+    
     def concat_alters(self, days):
         """
         Function to concat in list all updates, deletes, and inserts find in audit_logs
@@ -59,9 +62,9 @@ class Postgres:
         update = self.auditoperation_postgres('U', days)
         delete = self.auditoperation_postgres('D', days)
         inserts = self.auditoperation_postgres('I', days)
-
+        
         return update + delete + inserts
-
+    
     def list_to_dictionary(self, list_alters):
         """
         Function to get a list to be convert as dictionary final result
@@ -77,10 +80,9 @@ class Postgres:
             else:
                 values = i[6][1:-2].split(',')
                 list_to_dictionary.append([table_name, values[0], i[5]])
-
+        
         return list_to_dictionary
-
-
+    
     def get_alters(self, list_alters):
         """
         Function to get a dictionary of table names and primary key of inserts, deletes and updates
@@ -101,8 +103,47 @@ class Postgres:
                 dicionario[key][op] = [str(id_pk)]
             else:
                 dicionario[key][op].append(str(id_pk))
-
+        
         return dicionario
+    
+    def insert_sync_result(self, result):
+        """
+        Function to insert sync result of portal in postgres table
+        :param result: Dictionary of result about sync (need has same tables of create table)
+        :return: Dictionary inserted on postgres table
+        """
+        kwargs = result[0]['esri_fl']
+        kwargs.pop('fail_log')
+        kwargs['date'] = date.today()
+        self.create_sync_table()
+        insert_statement = self.sync_table.insert().values(**kwargs)
+        
+        return self.conn.execute(insert_statement)
+    
+    def create_sync_table(self):
+        """
+        Function to create table on postgres considering fields of sync script with portal
+        :return: Table create on postgres if not exists
+        """
+        meta = MetaData(self.conn_postgres())
+        kwargs = {"schema": 'audit'}
+        self.sync_table = Table('sync_portal', meta,
+                                Column('id_pk', Integer, autoincrement=True, primary_key=True),
+                                Column('feature_layer', String),
+                                Column('id', String),
+                                Column('total_added', Integer),
+                                Column('added_ids', postgresql.ARRAY(Integer)),
+                                Column('total_updated', Integer),
+                                Column('updated_ids', postgresql.ARRAY(Integer)),
+                                Column('total_deleted', Integer),
+                                Column('deleted_ids', postgresql.ARRAY(Integer)),
+                                Column('failed', Boolean),
+                                # Column('fail_log', postgresql.ARRAY(Integer)),
+                                Column('date', Date),
+                                **kwargs
+                                )
+        
+        return self.sync_table.create(checkfirst=True)
 
 
 if __name__ == '__main__':
@@ -111,3 +152,4 @@ if __name__ == '__main__':
     tables = pg.get_alters(list_alters)
     for item in tables.items():
         print(item)
+    pg.insert_sync_result(result)
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+from sqlalchemy import Table, MetaData
 from database.Postgresql import Postgres
 
 
@@ -42,3 +43,45 @@ def test_need_delete():
     result = set([i[5] for i in pg.auditoperation_postgres(choice)])
     
     assert result == {'D'} or result == set()
+
+
+def test_if_exists_sync_table():
+    """Confirm if sync table was created correctly on postgres"""
+    pg = Postgres()
+    meta = MetaData(pg.conn_postgres())
+    pg.create_sync_table()
+    variable = Table('sync_portal', meta, schema='audit')
+    
+    assert type(variable) == sqlalchemy.sql.schema.Table
+
+
+def test_inserts_portal():
+    """test if insert was send to postgres"""
+    pg = Postgres()
+    result = [
+        {
+            'wfs': {
+                "workspace": "geonode",
+                "layer_name": 'v_xyz_abc',
+                "total_features": 1234,
+                "readed": 123,
+                "feature_ids": [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+                "failed": True,
+                "fail_log": ['teste']
+            },
+            'esri_fl': {
+                "feature_layer": '/Hosted/Teste_result',
+                "id": '0',
+                "total_added": 1,
+                "added_ids": [11],
+                "total_updated": 2,
+                "updated_ids": [22, 33],
+                "total_deleted": 3,
+                "deleted_ids": [44, 55, 66],
+                "failed": True,
+                "fail_log": [1, 13, 'teste']
+            }
+        }
+    ]
+    
+    assert type(pg.insert_sync_result(result)) == sqlalchemy.engine.cursor.LegacyCursorResult
